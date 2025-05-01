eb717bcba0be85fc75a1f58c0297c8719128e941
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-05-01 19:01:19 +0200

    feat: definita variabile tasks e funzione addTask per aggiungere attività

---
+++ app.js
@@ -0,0 +1,16 @@
+let tasks = [];
+
+function addTask() {
+  const input = document.getElementById('taskInput');
+  const nome = input.value.trim();
+  if (nome === "") return;
+
+  tasks.push({
+    id: Date.now(),
+    nome,
+    stato: "Da fare"
+  });
+
+  input.value = "";
+  renderTasks();
+}
