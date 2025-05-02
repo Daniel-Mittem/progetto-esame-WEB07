a2753fb5b185976b53582eb68f3e4a71bb3d35c5
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-05-02 11:27:02 +0200

    feat: aggiungi funzione start

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let startTime = 0;
+let elapsedTime = 0;
+let interval = null;
+
 function formatTime(ms) {
     const totalMs = ms % 1000;
     const totalSeconds = Math.floor(ms / 1000);
@@ -12,5 +16,12 @@ function updateDisplay() {
     const now = Date.now();
     const diff = now - startTime + elapsedTime;
     document.getElementById('display').textContent = formatTime(diff);
+}
+
+function start() {
+  if (interval) return;
+
+  startTime = Date.now();
+  interval = setInterval(updateDisplay, 10);
 }
   
