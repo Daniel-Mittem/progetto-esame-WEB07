6628ab30c6ac46b27c6ed2ab01fd6472c6849887
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-05-03 23:40:01 +0200

    feat: aggiunta della logica per le icone del meteo

---
+++ app.js
@@ -0,0 +1,15 @@
+const weatherInfo = document.getElementById("weatherInfo");
+
+const weatherIcons = {
+  0: "☀️ Sole",
+  1: "🌤️ Parzialmente nuvoloso",
+  2: "⛅ Variabile",
+  3: "☁️ Nuvoloso",
+  45: "🌫️ Nebbia",
+  48: "🌫️ Nebbia congelante",
+  51: "🌦️ Pioggia leggera",
+  61: "🌧️ Pioggia moderata",
+  71: "❄️ Neve leggera",
+  95: "⛈️ Temporale",
+
+};
